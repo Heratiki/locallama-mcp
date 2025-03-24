6041cfeaeb0f33e100d0b2d67b215d7d4335449e
Heratiki <heratiki@gmail.com>
2025-03-24 13:40:05 -0400

    fix: enhance type safety in NewComponent by specifying DataItem for data handling methods

--- src/modules/new-module/newComponent.ts
+++ src/modules/new-module/newComponent.ts
@@ -1,17 +1,22 @@
-class NewComponent {
-  private data: any[] = [];
-  private dataMap: Map<string, any> = new Map();
+interface DataItem {
+  id: string;
+  [key: string]: string | number | boolean | null | undefined;
+}
 
-  public addData(item: any): void {
+class NewComponent {
+  private data: DataItem[] = [];
+  private dataMap: Map<string, DataItem> = new Map();
+
+  public addData(item: DataItem): void {
     this.data.push(item);
     this.dataMap.set(item.id, item);
   }
 
-  public getData(): any[] {
+  public getData(): DataItem[] {
     return this.data;
   }
 
-  public getDataById(id: string): any {
+  public getDataById(id: string): DataItem | undefined {
     return this.dataMap.get(id);
   }
 }
